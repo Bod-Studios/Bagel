b7b73bf9ffa0bdd26468c9bee522106ee582aa5e
Gravity-Developer <90658565+Gravity-Developer@users.noreply.github.com>
2022-01-09 07:59:34 -0800

    Turn Debug off for main module

--- src/bagel/config/config.py
+++ src/bagel/config/config.py
@@ -3,7 +3,7 @@ class Config:
     """Config Class for Copying to create the configuration object for bagel, can be left blank!
     """
     def __init__(self):
-        self.debug = True # Change for Production
+        self.debug = False # Change for Production
         self.gpu = False
         self.cpu = True
         self.id = str(uuid.uuid4())
