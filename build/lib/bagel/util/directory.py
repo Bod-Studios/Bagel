ec4fcb7f830d12cf85c3dbdcc8dd3c4ffd7c154c
Gravity-Developer <piderking8@gmail.com>
2022-01-11 19:11:57 +0000

    zipModuleImport creation

---
+++ build/lib/bagel/util/directory.py
@@ -0,0 +1,5 @@
+import os
+
+
+# This is for creating modules in the correct place
+src_path = os.path.abspath("../../")
--- src/bagel/core/__init__.py
+++ src/bagel/core/__init__.py
@@ -1,1 +1,4 @@
 from .webImport import webImport
+from .zipModuleImport import zipWebImport, deleteZipFiles
+
+# from gitModuleImport import ?
